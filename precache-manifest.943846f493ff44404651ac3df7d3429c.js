ae19633c23f159841dbd627a594fcb0ee2d0faa9
Erwin <erwin@dusdusan.com>
2020-02-05 01:56:22 +0700

    Updates

---
+++ precache-manifest.943846f493ff44404651ac3df7d3429c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc9d1cadd14d1bcd22dec4ca912412e",
+    "url": "/fabelio_test/./index.html"
+  },
+  {
+    "revision": "bdfdc602fa1c6a856f7b",
+    "url": "/fabelio_test/./static/css/2.b650302e.chunk.css"
+  },
+  {
+    "revision": "c96e95777b8707a9838b",
+    "url": "/fabelio_test/./static/css/main.e2678527.chunk.css"
+  },
+  {
+    "revision": "bdfdc602fa1c6a856f7b",
+    "url": "/fabelio_test/./static/js/2.34d48fe1.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/fabelio_test/./static/js/2.34d48fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96e95777b8707a9838b",
+    "url": "/fabelio_test/./static/js/main.08ffc095.chunk.js"
+  },
+  {
+    "revision": "b92da57ce1415447bd4f",
+    "url": "/fabelio_test/./static/js/runtime-main.b615dd87.js"
+  }
+]);
